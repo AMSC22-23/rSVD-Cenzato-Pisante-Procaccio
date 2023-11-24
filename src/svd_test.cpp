cb213d4dae068367045ee543039048f0ec8b51cc
ariii123 <arianna.procaccio@mail.polimi.it>
2023-11-24 13:14:28 +0000

    small changes

--- src/svd_test.cpp
+++ src/svd_test.cpp
@@ -1,41 +1,40 @@
-#include <Eigen/Dense>
-#include <iostream>
-
-using Vector=Eigen::VectorXd;
-using Matrix=Eigen::MatrixXd;
-
 #include "svd_with_pm.hpp"
 
 int main(){
 
     // Test matrix
-    Matrix A(4,3);
+    /*Matrix A(4,3);
     A << 1,2,3,
         4,5,6,
         7,8,9,
-        10,11,12;
+        10,11,12;*/
 
-    /*Matrix A(5,5);
+    Matrix A(5,5);
     A << 2,-1,0,0,0,
         -1,2,-1,0,0,
         0,-1,2,-1,0,
         0,0,-1,2,-1,
-        0,0,0,-1,2;*/
+        0,0,0,-1,2;
 
     int m=A.rows(), n=A.cols();
 
-    // Definitions of matrices
-    Matrix U(m,m),V(n,n);
+    Matrix U(m,m),V(n,n),S=Matrix::Zero(m,n);
     Vector s(n);
-    double m_eps=1e-10, m_delta = 0.1, m_landa = 0.1; //da capire come settare parametri
+    double m_eps=1e-12;        
 
-    svd obj(m_eps, m_delta, m_landa);
+    SVD obj(m_eps);
     obj.svd_with_PM(A,U,s,V);
 
     std::cout<<"\nSVD :\n";
     std::cout<<U<<std::endl;
     std::cout<<s<<std::endl;
     std::cout<<V<<std::endl;
+
+    for(int i=0;i<A.cols();i++){
+        S(i,i)=s[i];
+    }
+
+    std::cout<<"\n"<<U*S*V.transpose()<<std::endl;
 
     return 0;
 }
--- src/svd_with_pm.hpp
+++ src/svd_with_pm.hpp
@@ -8,70 +8,85 @@
 using Vector=Eigen::VectorXd;
 using Matrix=Eigen::MatrixXd;
 
-class svd{
+class SVD{
     public:
-    /* Constructor 
-            inputs : precision (eps) with probability 1-delta 
-                     and landa */
-        svd(double eps, double delta, double landa) : 
-        m_eps(eps), m_delta(delta), m_landa(landa) {}
-
-    /* Power Method */
-    void PM(Matrix A,double h,Vector x, Vector &u, Vector &v, double &sigma){
-        for(int i=0;i<int(h);i++){
-            x = A.transpose() * A * x;
-            x = x / x.norm();
-        }
-        v = x;
-        sigma = (A * v).norm();
-        u = A * v / sigma;
-    }
+    /* Constructor : 
+            epsilon : precision of power method  */
+        SVD(double epsilon) : 
+        m_epsilon(epsilon) {}
 
     /* Generates random vector of n elem with normal distribution */
     Vector genvec(int n){
         Vector v(n);
-        std::default_random_engine gen;
+        //std::default_random_engine gen;
+        std::random_device rd ;
+        std::knuth_b reng{rd ()};
         std::normal_distribution<> dice(0);
         for(int i=0;i<n;i++){
-            v(i) = dice(gen);
+            v(i) = dice(reng);
         }
         return v;
     }
 
     /* SVD with Power Method :
-        input: A, U, s, V
-        A is the matrix on which the SVD is performed
-        s is as a vector containing singular values
-        U and V are matrices containing right and left singular vectors*/
-    void svd_with_PM(Matrix A, Matrix &U, Vector &s,Matrix &V){
-        int n=A.cols();
-        int m=A.rows();
-        for(int i=0;i<n;i++){                     // n < m 
-            Vector x0 = genvec(n);                //initial guess 
-            Vector u(m), v(n);
-            double sigma;
-            double h = log(4*log(2*n/m_delta)/(m_eps*m_delta))/(2*m_landa);
-            //double h=5;
-            std::cout<<h<<std::endl;
-            PM(A,h,x0,u,v,sigma);
+            A (m x n) : the matrix on which the SVD is performed
+            s (n)     : vector containing singular values
+            U (m x m) : right singular vectors
+            V (n x n) : left singular vectors*/
+    void svd_with_PM(Matrix A, Matrix &U, Vector &s, Matrix &V){
+        int n=A.cols(), m=A.rows(), i=0;
+        Matrix B(n,n);
+        Vector u(m), v(n), x0(n);
+        double sigma=1.;
+        //double h = log(4*log(2*n/m_delta)/(m_epsilon*m_delta))/(2*m_landa);
+        while((abs(sigma) > m_epsilon) && i<n){                      
+            x0 = genvec(n);            
+            //std::cout<<x0<<"\n\n";
+            B =  A.transpose() * A;
+            v = PowerMethod(B, x0);
+            sigma = norm(A * v);
+            u = A * v / sigma;
 
             U.col(i) = u;           
             s[i]=sigma;
             V.col(i) = v; 
             
             A = A - sigma * u * v.transpose(); 
-            std::cout<<h<<std::endl;
-            std::cout<<U<<std::endl;
+            std::cout<<A<<std::endl<<std::endl;
+            i++;
         }
     }
 
     /* Destructor */
-        ~svd() = default;
+        ~SVD() = default;
+
 
     private:
-        const double m_eps;
-        const double m_delta;
-        const double m_landa;
+
+    /* Power Method :
+            B (n x n) = At * A
+            x (n) = initial guess
+            Output :
+            v (n) : x converges to the left singular vector of A  */
+    Vector PowerMethod(Matrix B, Vector x){
+        Vector xold=Vector::Ones(x.size());
+        while( norm(xold-x) > m_epsilon ){
+            xold=x;
+            x = B * x;
+            x = x / norm(x);
+        }
+        return x;
+    }
+
+    double norm(Vector v){
+        double norm=0;
+        for(int i=0;i<v.size();i++){
+            norm += v[i] * v[i];
+        }
+        return sqrt(norm);
+    }
+
+    const double m_epsilon;
 };
 
 #endif // SVD_WITH_PM_HPP
