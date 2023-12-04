4b6a0b1d1465376aa88a49ecc6af717848deba11
GiuseppePisante <gpisante01@gmail.com>
2023-12-04 16:40:42 +0000

    improved parallel algorithm for Givens method

--- src/QR_DecompositionTest.cpp
+++ src/QR_DecompositionTest.cpp
@@ -69,7 +69,7 @@ Matrix A(3,3);
     /**
      * Parallel execution on OpenMP
     */
- /*   
+   
     auto start_parallel = std::chrono::high_resolution_clock::now();
     auto [Qp,Rp]=QR_A.Givens_solve_parallel(A);
     auto end_parallel = std::chrono::high_resolution_clock::now();
@@ -81,14 +81,14 @@ Matrix A(3,3);
     std::cout<<"Q_parallel="<<std::endl;
     std::cout<<Qp<<std::endl;
     std::cout << "Time of execution: " << duration_p.count() << " s" << std::endl;
-*/
 
+/* 
     auto [Qh,Rh]=QR_A.HouseHolder_solve(A);
     std::cout<<"R_Serial="<<std::endl;
     std::cout<<Rh<<std::endl;
     std::cout<<"Q_Serial="<<std::endl;
     std::cout<<Qh<<std::endl;
-    
+*/    
 
 
 	return 0;
--- src/QR_Decomposition_parallel.cpp
+++ src/QR_Decomposition_parallel.cpp
@@ -21,76 +21,75 @@ std::tuple<Matrix, Matrix> QR_Decomposition::Givens_solve_parallel(Matrix A){
     * Assembling the matrix Q by applying the Givens rotation at each 
     * iteration and applying each component of Q to R in order to make it triangular
     */
-    int count=1;
-
     
+    for (int j = 0;j<n;j++){
+        for(int i=m-1;i>j;i--){
+
+            /**
+                * Givens rotation gets applied by calculating the values of:
+                * c: cosine of the angle of rotation
+                * s: sine of the angle of rotation
+                * 
+                * The idea is to calculate the rotations on smaller vectors, iterating on the cells 
+                * below the diagonal to pull them to zero
+            */
+            
+            double a=R.coeffRef(i-1,j);
+            double b=R.coeffRef(i,j);
+            double c,s;
         
-        for (int j = 0;j<n;j++){
-            for(int i=m-1;i>j;i--){
-
-                /**
-                    * Givens rotation gets applied by calculating the values of:
-                    * c: cosine of the angle of rotation
-                    * s: sine of the angle of rotation
-                    * 
-                    * The idea is to calculate the rotations on smaller vectors, iterating on the cells 
-                    * below the diagonal to pull them to zero
-                */
-                
-                double a=R.coeffRef(i-1,j);
-                double b=R.coeffRef(i,j);
-                double c,s;
-            
-                if (abs(a)>abs(b) ){
-                    if(a!=0.0){
-                        int segno = std::signbit(a) ? -1 : 1;
-                        c = segno / sqrt(1+(b/a)*(b/a));
-                        s  = abs(c/a)*b;
-                        } else{
-                            c=0.0;
-                            s=(a >= 0.0 ? 1.0 : -1.0);
-                        }
-                    }else if (b!=0.0)  {
-                        int segno = (std::signbit(b) ? -1 : 1);
-                        s = segno / sqrt(1+(a/b)*(a/b));
-                        c=abs(s/b)*a;
-                        } else{
-                            s=0.0;
-                            c=(b >= 0.0 ? 1.0 : -1.0);
+            if (abs(a)>abs(b) ){
+                if(a!=0.0){
+                    int segno = std::signbit(a) ? -1 : 1;
+                    c = segno / sqrt(1+(b/a)*(b/a));
+                    s  = abs(c/a)*b;
+                    } else{
+                        c=0.0;
+                        s=(a >= 0.0 ? 1.0 : -1.0);
                     }
+                }else if (b!=0.0)  {
+                    int segno = (std::signbit(b) ? -1 : 1);
+                    s = segno / sqrt(1+(a/b)*(a/b));
+                    c=abs(s/b)*a;
+                    } else{
+                        s=0.0;
+                        c=(b >= 0.0 ? 1.0 : -1.0);
+                }
 
+        
+            /**
+                * Instead of creating the Givens matrix, I do directly the computation on Q and R
+                * In addition I use a temporal variable to avoid changing the matrix before the computation
+                * Since there is dependencies within the for loop, I use atomic add
+            */
             
-                /**
-                    * Instead of creating the Givens matrix, I do directly the computation on Q and R
-                    * In addition I use a temporal variable to avoid changing the matrix before the computation
-                    * Since there is dependencies within the for loop, I use atomic add
-                */
-                
-                double tmp = 0.0;
-                #pragma omp parallel for
-                for (int k = 0; k < n; k++) {
-                    tmp = c * R.coeffRef(i - 1, k) + s * R.coeffRef(i, k);
-                    R.coeffRef(i, k) = -s * R.coeffRef(i - 1, k) + c * R.coeffRef(i, k);
-                    R.coeffRef(i - 1, k) = tmp;
-                }
-                
-                R.coeffRef(i, j) = 0;
-                #pragma omp parallel for
-                for (int k = 0; k < m; k++) {
-                    tmp = Q.coeffRef(k, i - 1) * c + Q.coeffRef(k, i) * s;
-                    Q.coeffRef(k, i) = Q.coeffRef(k, i - 1) * -s + Q.coeffRef(k, i) * c;
-                    Q.coeffRef(k, i - 1) = tmp;
-                }
+            double tmp = 0.0;
+            #pragma omp simd
+            for (int k = 0; k < n; k++) {
+                tmp = c * R.coeffRef(i - 1, k) + s * R.coeffRef(i, k);
+                R.coeffRef(i, k) = -s * R.coeffRef(i - 1, k) + c * R.coeffRef(i, k);
+                R.coeffRef(i - 1, k) = tmp;
+            }
+            
+            R.coeffRef(i, j) = 0;
+            #pragma omp simd
+            for (int k = 0; k < m; k++) {
+                tmp = Q.coeffRef(k, i - 1) * c + Q.coeffRef(k, i) * s;
+                Q.coeffRef(k, i) = Q.coeffRef(k, i - 1) * -s + Q.coeffRef(k, i) * c;
+                Q.coeffRef(k, i - 1) = tmp;
             }
         }
-    
+    }
 
-    #pragma omp parallel for
+
+    
         for (int i = 0; i < n - 1; i++) {
             if (R(i, i) > 0) {
+                #pragma omp simd
                 for (int j = i; j < n; j++) {
                     R.coeffRef(i, j) = -R.coeffRef(i, j);
                 }
+                #pragma omp simd
                 for (int j = 0; j < m; j++) {
                     Q.coeffRef(j, i) = -Q.coeffRef(j, i);
                 }
